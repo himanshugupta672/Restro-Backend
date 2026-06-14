6852f8a0afff7199083a79581d0cce678aaa8e69
Himanshu Gupta <himanshugupta0669@gmail.com>
2026-06-14 15:39:40 +0530

    Enable swagger for azure deployment

--- Restaurant.API/Program.cs
+++ Restaurant.API/Program.cs
@@ -135,9 +135,12 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    //app.UseSwagger();
+    //app.UseSwaggerUI();
 }
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
